843da29f26af9ad67587b3158302e6d92b5cc903
helena rodrigues <helena19.rodrigues@gmail.com>
2022-04-26 18:50:59 +0100

    Js del 1 al 5

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("¡Empezamos con javascript!");
+console.log("Hola soy un estudiante Hackademy");
+let vacaciones="En Hawai";
+console.log(vacaciones)
+vacaciones="En casa";
+console.log(vacaciones);
+let text1="Hola! me gusta la fruta, y a ti?";
+let text2="A mi tambien me encanta la fruta";
+console.log(text1, text2);
